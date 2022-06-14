34ae9491fc887a6830faf426f2b9cf3079b7bf65
RobARC <rrondoc@gmail.com>
2022-06-14 09:47:13 -0500

    Updating app, adding route time line

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -13,74 +13,77 @@ admin.initializeApp({
 const db = admin.database();
 
 router.get('/', async (req, res) => {
-
-    await db.ref('tweets').on('value', (snapshot) => {
-        const data = snapshot.val();
-        let crx = [];
-        snapshot.forEach(element => {
-            crx = element.val();
-            //console.log(element.val()[0].text);
+    try {
+        await db.ref('tweets').on('value', (snapshot) => {
+            const data = snapshot.val();
+            let crx = [];
+            snapshot.forEach(element => {
+                crx = element.val();
+                //console.log(element.val()[0].text);
+            });
+            res.render('home', { tweets: crx });
         });
-        res.render('home', { tweets: crx });
-    });
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+    }
 });
-
 
 router.get('/search/:word', async (req, res) => {
 
     const params = { q: req.params.word, count: 5, result_type: 'recent', lang: 'es' }
 
-    await ApiClient.ApiClient.get('search/tweets', params, function (err, data, response) {
+    await ApiClient.ApiClient.get('search/tweets', params, async function (err, data, response) {
         //console.log(data);
         const allTweetsDto = data.statuses.map(tweet => tweetToDto(tweet));
-        res.json(allTweetsDto)
+        //res.json(allTweetsDto)
+        await saveInDB(allTweetsDto);
+        await res.redirect('/');
     });
 });
 
-
 router.post('/search/:word', async (req, res) => {
+    try {
+        const params = { q: req.params.word, count: 5, result_type: 'recent', lang: 'es' };
 
-    const params = { q: req.params.word, count: 5, result_type: 'recent', lang: 'es' }
+        await ApiClient.ApiClient.get('search/tweets', params, async function (err, data, response) {
 
-    await ApiClient.ApiClient.get('search/tweets', params, async function (err, data, response) {
-        try {
             //transfor tweets in a smaller data
             const allTweetsDto = data.statuses.map(tweet => tweetToDto(tweet));
             await saveInDB(allTweetsDto);
             await res.redirect('/');
-        }
-        catch (err) {
-            console.log(err);
-        }
 
-    });
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+    }
+
 });
 
-router.get('/statuses/user_timeline/', async (req, res) => {
-    const params = { screen_name: 'TalCual', count: 5 }
+router.get('/statuses/user_timeline/:screen_name', async (req, res) => {
+    try {
+        const params = { screen_name: req.params.screen_name, count: 5 }
 
-    twitterClient.twitterClient.get('/statuses/user_timeline/', params, function (err, data, response) {
-        //const tweets = [];
+        await twitterClient.twitterClient.get('/statuses/user_timeline/', params, async function (err, data, response) {
+            //await console.log(data);
+            const allTweetsTimeLine = data.map(tweets => tweetsTimeLine(tweets));
 
-        console.log(data)
-
-        //tweets.push(data);
-        //tweetToDto(data);
-
-        //console.log(tweets);
-    })
-
-    //const allTweetsDto = data
-
-    //console.log(allTweetsDto);
-    //await saveInDB(allTweetsDto);
-    //await res.redirect('/');
-
-
-})
-
-
-
+            //console.log(allTweetsTimeLine);
+            await saveInDB(allTweetsTimeLine);
+            await res.redirect('/');
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+    }
+});
 
 //Delete all Tweets
 router.get('/delete-tweet/', async (req, res) => {
@@ -90,6 +93,8 @@ router.get('/delete-tweet/', async (req, res) => {
     }
     catch (err) {
         console.log(err);
+        res.sendStatus(500);
+        return;
     }
 });
 
@@ -106,7 +111,22 @@ function tweetToDto(tweet) {
         description: tweet.user.description,
         profile_img: tweet.user.profile_image_url_https
     };
+
 };
+
+//Data to return TimeLine 
+function tweetsTimeLine(tweets) {
+    return {
+        created_at: tweets.created_at,
+        text: tweets.text,
+        name: tweets.user.name,
+        screen_name: tweets.user.screen_name,
+        id: tweets.user.id,
+        location: tweets.user.location,
+        description: tweets.user.description,
+        profile_img: tweets.user.profile_image_url_https
+    };
+}
 
 //save in Firebase database
 async function saveInDB(allTweetsDto) {
@@ -114,6 +134,7 @@ async function saveInDB(allTweetsDto) {
     console.log('Saved in Firebase');
 };
 
+//delete data from DB firebase
 async function deleteTwits() {
     await db.ref('tweets').remove();
     console.log('Removed in Firebase');
